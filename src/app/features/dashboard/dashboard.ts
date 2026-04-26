eb3baf8f66eed7729aaec48d152e5d5cd9b18e8c
moussabd <109081623+JawkAWUME@users.noreply.github.com>
2026-04-26 21:41:34 +0000

    push 21H41

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -637,4 +637,16 @@ onSaved(response?: any): void {
   togglePasswordDetail(): void {
     this.showPasswordDetail.set(!this.showPasswordDetail());
   }
+
+  downloadConfig(content: string, filename: string): void {
+    const blob = new Blob([content], { type: 'text/plain;charset=utf-8' });
+    const url  = URL.createObjectURL(blob);
+    const a    = document.createElement('a');
+    a.href     = url;
+    a.download = filename;
+    a.click();
+    URL.revokeObjectURL(url);
+}
+
+
 }
